d2d5af9db25691efb30e972ad1f3e088dfba52f4
Rudolf Amirian <ronaldo679@gmail.com>
2023-11-29 17:19:28 +0300

    VC for library screen

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+final class AuthManager {
+    static let shared = AuthManager()
+    
+    struct Constants {
+        static let clientID = "1676bc5bcd824659b8f77c09b4126f97"
+        static let clientSecret = "fc35b1f9e44246b5afa3c80956ddafc6"
+    }
+    
+    private init() {}
+    
+    public var signInUrl: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    
+    }
+    
+    
+    
+    var isSignedIn: Bool {
+        return false
+    }
+    
+    private var accessToken: String? {
+        return nil
+    }
+    
+    private var refreshToken: String? {
+        return nil
+    }
+    
+    private var tokenExpirationDate: Date? {
+        return nil
+    }
+    
+    private var shouldRefreshToken: Bool {
+        return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        //Get token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
+    }
+}
