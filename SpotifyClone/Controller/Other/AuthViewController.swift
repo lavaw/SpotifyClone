3b85c7c83276bbc03e206989f8f326a56d0b630d
Rudolf Amirian <ronaldo679@gmail.com>
2023-12-01 03:44:38 +0300

    Added AuthResponse forread response from spotify

--- SpotifyClone/Controller/Other/AuthViewController.swift
+++ SpotifyClone/Controller/Other/AuthViewController.swift
@@ -53,6 +53,12 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
             return
         }
         print("Code: \(code)")
+        AuthManager.shared.exchangeCodeForToken(code: code) { [weak self] success in
+            DispatchQueue.main.async {
+                self?.navigationController?.popToRootViewController(animated: true)
+                self?.completionHandler?(success)
+            }
+        }
     }
    
 
