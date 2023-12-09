52bd0c9e6482617878099c7b06950a20669ea236
Rudolf Amirian <ronaldo679@gmail.com>
2023-12-09 03:16:51 +0300

    added playlists and recommendations

---
+++ SpotifyClone/ViewModels/FeaturedPlaylistCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeaturedPlaylistCellViewModel.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 09.12.23.
+//
+
+import Foundation
---
+++ SpotifyClone/ViewModels/RecommndedTrackListCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecommndedTrackListCellViewModel.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 09.12.23.
+//
+
+import Foundation
