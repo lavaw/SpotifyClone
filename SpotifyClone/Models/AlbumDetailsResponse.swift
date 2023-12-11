d4e842529e56a17b93f312551a11b6b2bc6f8de4
Rudolf Amirian <ronaldo679@gmail.com>
2023-12-11 18:53:51 +0300

    added apicall for playlist and album details

---
+++ SpotifyClone/Models/AlbumDetailsResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AlbumDetailsResponse.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 11.12.23.
+//
+
+import Foundation
---
+++ SpotifyClone/Models/PlaylistsDetailsResponse.swift
@@ -0,0 +1,8 @@
+//
+//  PlaylistsDetailsResponse.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 11.12.23.
+//
+
+import Foundation
