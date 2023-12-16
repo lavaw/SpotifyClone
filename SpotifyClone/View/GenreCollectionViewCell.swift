d36290f68837ec857c78884a1a6e2c2bfa4a4a7d
Rudolf Amirian <ronaldo679@gmail.com>
2023-12-16 16:15:28 +0300

    Preape Search UI

---
+++ SpotifyClone/View/GenreCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GenreCollectionViewCell.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 16.12.23.
+//
+
+import UIKit
+
+class GenreCollectionViewCell: UICollectionViewCell {
+    
+}
