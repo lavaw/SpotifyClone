1f550d7c283d5491fe503f5cbed1111c2b828ed1
Rudolf Amirian <ronaldo679@gmail.com>
2023-12-18 04:11:26 +0300

    Categories VC added

---
+++ SpotifyClone/Models/AllCategoriesResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AllCategoriesResponse.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 18.12.23.
+//
+
+import Foundation
---
+++ SpotifyClone/ViewModels/CategoryCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryCollectionViewCellViewModel.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 18.12.23.
+//
+
+import Foundation
