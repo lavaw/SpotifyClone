486c515316da9fe8d88b01e3ed7d0422e1ea1d3d
Rudolf Amirian <ronaldo679@gmail.com>
2023-12-18 04:11:56 +0300

    API call for categories from searchVC

--- SpotifyClone/Models/AllCategoriesResponse.swift
+++ SpotifyClone/Models/AllCategoriesResponse.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct AllCategoriesResponse: Codable {
+    let categories: Categories
+}
+
+struct Categories: Codable {
+    let items: [Category]
+}
+
+struct Category: Codable {
+    let id: String
+    let name: String
+    let icons: [APIImage]
+}
+
