45047033c82f772b4e5d0b243b6e54412f50b32f
Rudolf Amirian <ronaldo679@gmail.com>
2023-12-20 02:59:59 +0300

    edited search api

---
+++ SpotifyClone/Models/SearchResult.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResult.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 20.12.23.
+//
+
+import Foundation
---
+++ SpotifyClone/Models/SearchResultResponse.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResultResponse.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 20.12.23.
+//
+
+import Foundation
