ff953a343fa5299d12d7b2ce523f626c570b5999
Rudolf Amirian <ronaldo679@gmail.com>
2023-12-24 16:20:32 +0300

    config buttons on playerVC

--- SpotifyClone/Controller/Other/PlayerViewController.swift
+++ SpotifyClone/Controller/Other/PlayerViewController.swift
@@ -16,16 +16,24 @@ class PlayerViewController: UIViewController {
         return imageView
     }()
     
+    private let controlsView = PlayerControlsView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(imageVIew)
+        view.addSubview(controlsView)
+        controlsView.delegate = self
         configureBarButtons()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageVIew.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.width, height: view.width)
+        controlsView.frame = CGRect(x: 10,
+                                    y: imageVIew.buttom+10,
+                                    width: view.width-20,
+                                    height: view.height-imageVIew.height-view.safeAreaInsets.top-view.safeAreaInsets.bottom-15)
     }
     
     private func configureBarButtons() {
@@ -41,5 +49,21 @@ class PlayerViewController: UIViewController {
     @objc private func didTapAction() {
         // Actions
     }
+    
+}
+
+extension PlayerViewController: PlayerControlsViewDelegate {
+    func playerCotrolsViewDidTapPlayPauseButton(_ playerControlsView: PlayerControlsView) {
+        <#code#>
+    }
+    
+    func playerCotrolsViewDidTapFrowardButton(_ playerControlsView: PlayerControlsView) {
+        <#code#>
+    }
+    
+    func playerCotrolsViewDidTapBackwardButton(_ playerControlsView: PlayerControlsView) {
+        <#code#>
+    }
+    
     
 }
