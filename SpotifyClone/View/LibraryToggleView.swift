997cfcfc1acb783cc0a44a41e689e4377382ad4f
Rudolf Amirian <ronaldo679@gmail.com>
2024-01-07 16:13:00 +0300

    toggle for playlist and albums buttons

---
+++ SpotifyClone/View/LibraryToggleView.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryToggleView.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 07.01.24.
+//
+
+import UIKit
+
+class LibraryToggleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
