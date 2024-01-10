e233dd2008b73dd3088ea29814e3ad2dbf22a483
Rudolf Amirian <ronaldo679@gmail.com>
2024-01-10 17:02:19 +0300

    preparing libraryVC to show info

---
+++ SpotifyClone/Models/LibraryPlaylistsResponse.swift
@@ -0,0 +1,8 @@
+//
+//  LibraryPlaylistsResponse.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 10.01.24.
+//
+
+import Foundation
---
+++ SpotifyClone/View/ActionLabelView.swift
@@ -0,0 +1,20 @@
+//
+//  ActionLabelView.swift
+//  SpotifyClone
+//
+//  Created by Rudolf Amiryan on 10.01.24.
+//
+
+import UIKit
+
+class ActionLabelView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
