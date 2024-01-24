dad52b545db20a8c9515e6c2be840250cebf6d94
Rudolf Amirian <ronaldo679@gmail.com>
2024-01-25 02:54:44 +0300

    added haptic notification when select a row

--- SpotifyClone/Controller/Other/Library/LibraryPlaylistsViewController.swift
+++ SpotifyClone/Controller/Other/Library/LibraryPlaylistsViewController.swift
@@ -96,9 +96,11 @@ class LibraryPlaylistsViewController: UIViewController {
             
             APICaller.shared.createPlaylist(with: text) { [weak self] success in
                 if success {
+                    HapticsManager.shared.vibrate(for: .success)
                     // Refresh list of playlists
                     self?.fetchData()
                 } else {
+                    HapticsManager.shared.vibrate(for: .error)
                     print("Failed to create playlist")
                 }
             }
@@ -134,6 +136,7 @@ extension LibraryPlaylistsViewController: UITableViewDelegate, UITableViewDataSo
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        HapticsManager.shared.vibrateForSelection()
         let playlist = playlists[indexPath.row]
         guard selectionHandler == nil else {
             selectionHandler?(playlist)
